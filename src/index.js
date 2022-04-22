6b9b5f97cc1396cd8939a84bc97f5c04385625c2
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-22 10:10:09 -0300

    Tipos de parâmetros

--- src/index.js
+++ src/index.js
@@ -2,15 +2,42 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+/**
+ * GET - Buscar uma informação dentro do servidor
+ * POST - Inserir uma informação no servidor
+ * PUT - Alterar uma informação no servidor
+ * PATCH - Alterar uma informação específica
+ * DELETE - Deletar uma informação no servidor
+ */
+
+/**
+ * Tipos de parâmetros
+ * 
+ * Route Params => Identificar um recurso editar/deletar/buscar
+ * Query Params => Paginação / Filtro
+ * Body Params => Os objetos inserção/alteração (JSON)
+ */
+
 app.get('/courses', (request, response) => {
+  const query = request.query;
+  console.log(query);
+
   return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post('/courses', (request, response) => {
+  const body = request.body;
+  console.log(body);
+
   return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put('/courses/:id', (request, response) => {
+  const params = request.params;
+  console.log(params);
+
   return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
